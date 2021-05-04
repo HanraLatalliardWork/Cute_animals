2a099fa1995772a66b7b2b1e3d86b632bf2831f3
Hanra Latalliard <73407484+HanraLatalliardWork@users.noreply.github.com>
2021-05-04 10:43:29 +0200

    Add files via upload

---
+++ files/actions/redirect.js
@@ -0,0 +1,6 @@
+function timeoutRedirect(link="https://hanralatalliardwork.github.io/Cute_animals/",time=2000){
+	setTimeout('RedirectIt(link)', time);
+}
+function RedirectIt(link){
+  document.location.href=`${link}`; 
+}
