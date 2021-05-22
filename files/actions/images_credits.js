0687f71a2fe2551a30593e649aae0cf85b88fd9b
Hanra Latalliard <73407484+HanraLatalliardWork@users.noreply.github.com>
2021-05-22 11:50:21 +0200

    Update images_credits.js

--- files/actions/images_credits.js
+++ files/actions/images_credits.js
@@ -22,7 +22,7 @@ function putImagesLinks(lst,folder="",lstLink,IDTo,tip){
     Alphabet=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
     AlphabetInt=0,
     div ="";
-    div+=document.getElementById(IDTo);
+    //div+=document.getElementById(IDTo);
     if (tip===1){Alphabet=0};
     if (tip===2){AlphabetInt=0};
     for (var i=0;i<lst.length;i++){
